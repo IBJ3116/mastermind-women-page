09231dcfbc76e737dee1b1b7de5346b2193d838f
IBJ <127997144+IBJ3116@users.noreply.github.com>
2024-05-12 13:55:11 +0100

    Thrid

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -6,7 +6,7 @@ import React from 'react'
 const Hero = () => {
   return (
     <main className='relative items-center h-full w-full mt-[112px] lg:ml-[180px] lg:max-w-[1000px] lg:mt-[100px] pl-[32px]'>
-        <h1 id='Herotitle' className="clashDisplayMediumFont lg:text-[96px] leading-[115%] w-full lg:max-w-[864px] text-[48px]">
+        <h1 id='Herotitle' className="lg:text-[96px] leading-[115%] w-full lg:max-w-[864px] text-[48px]">
         Empowering <br /> <span className='text-[#00CC57] lg:max-w-[864px] w-fit'>Women & Children</span>
         </h1>
         <p className='max-w-[363px] lg:max-w-[773px]  text-[14px] lg:text-[24px] mb-[20px]'>Single mothers are not a forgotten breed at MMR. With combined efforts, you can help us lead women and children to eventually live a self-reliant lifestyle.</p>
